2e7329be55aa6c8e5fd6c90d4e92fdd808c3d1f1
theMA <theMA@DESKTOP-C2B1C5R>
2022-05-02 17:33:47 -0400

    Agregar archivos de proyecto.

---
+++ hilos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hilos
+{
+    class Program
+    {
+        static void metodo()
+        {
+            String texto = Thread.CurrentThread.Name;
+            Console.WriteLine(texto);
+            Thread.Sleep(3000);
+            for (int x = 0; x <= 1000; x++)
+            {
+                Console.Write('x');
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(metodo));
+            t.Name = "hilo1";
+            t.Start();
+            for (int x = 0; x <= 1000; x++)
+            {
+                Console.Write('y');
+            }
+            Console.ReadKey();
+        }
+    }
+}
