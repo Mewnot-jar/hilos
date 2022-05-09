954ffb3a191cb06e6a9e4d68819212b5ba37a2dc
Mewnot <72050891+Mewnot-jar@users.noreply.github.com>
2022-05-09 17:49:53 -0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numeros = new int[4];
+            Boolean pasa = true;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Ingrese el digito {0}: ", i+1);
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    if (numeros[i] == numeros[i+1] || numeros[i] == numeros[i + 2] || numeros[i] == numeros[i + 3])
+                    {
+                        pasa = false;
+                    }
+                }
+                if (i == 1 && pasa == true)
+                {
+                    if (numeros[i] == numeros[i - 1] || numeros[i] == numeros[i + 1] || numeros[i] == numeros[i + 2])
+                    {
+                        pasa = false;
+                    }
+                }
+                if(i == 2 && pasa == true)
+                {
+
+                    if (numeros[i] == numeros[i-2] || numeros[i] == numeros[i-1] || numeros[i] == numeros[i + 1])
+                    {
+                        pasa = false;
+                    }
+                }
+                if(i == 3 && pasa == true)
+                {
+                    if (numeros[i] == numeros[i-3] || numeros[i] == numeros[i-2] || numeros[i] == numeros[i-1])
+                    {
+                        pasa = false;
+                    }
+                }
+               
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if(numeros[i] == i+1)
+                {
+                    pasa = false;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0 || i == 1 || i == 2)
+                {
+                    if (numeros[i] == numeros[i + 1])
+                    {
+                        pasa = false;
+                    }
+                }
+                else
+                {
+                    if (numeros[i] % 2 == 1)
+                    {
+                        pasa = false;
+                    }
+                }
+            }
+            if (pasa == true)
+            {
+                Console.WriteLine("Correcto");
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+            }
+            Console.ReadKey();
+        }
+    }
+}
